c1a25c20a0559c0ef50a0d76b8e580607d4fd219
yatemmma <yatemmma@gmail.com>
2015-03-14 20:59:06 +0900

    remove preview page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD']
+  http_basic_authenticate_with(
+  	:name => ENV['BASIC_AUTH_USERNAME'], 
+  	:password => ENV['BASIC_AUTH_PASSWORD']
+  ) unless ENV['BASIC_AUTH_USERNAME'].nil?
 end
