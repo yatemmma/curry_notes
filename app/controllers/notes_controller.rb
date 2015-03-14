48d99c4e6d5b44a72dfee545f5b7868368523e5b
yatemmma <yatemmma@gmail.com>
2015-03-14 22:04:29 +0900

    sort index

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,7 +4,7 @@ class NotesController < ApplicationController
   # GET /notes
   # GET /notes.json
   def index
-    @notes = Note.all
+    @notes = Note.order(:id).reverse
   end
 
   # GET /notes/1
